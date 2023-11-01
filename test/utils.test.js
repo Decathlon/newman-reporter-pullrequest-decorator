b28f6f9394495427eadaebc47790061701e476d9
erowley7 <141340672+erowley7@users.noreply.github.com>
2023-11-01 12:25:56 +0000

    fix:Adding comment link to checks, re-worded the comment (#14)
    fix: Improving pull request comment (content + link) and avoid raising an error when there is no pull request matching the provided commit.

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,7 @@
 const { buildMarkdownText } = require("../lib/utils.js")
 const {buildPullRequestComment} = require("../lib/utils.js")
+const GithubService = require('../lib/github-service.js')
+
 chai = require('chai'),
 	{ assert, expect } = chai,
 	path = require('path'),
@@ -216,30 +218,77 @@ describe("Markdown generation", function () {
 });
 
 describe("Utils : buildPullRequestComment", () => {
-    it("Given a collection with no tests, return the correct comment", () => {
-        const expectedResult = `**Summary**:  \n  ❌ 0 **Tests Failed**\n  ⏩ 0 **Tests Skipped**\n  ✅ 0 / 0 **Reqests Passed**`;
 
+	let githubService = new GithubService({
+		token: "anyToken",
+		repo: "organization/repo",
+		checkName: "non-regression-test-1",
+		refCommit: "2d58379528f086352820a3f534396728dad4353f",
+
+	});
+
+    it("Given a collection with no tests, return the correct comment", () => {
+        const expectedResult = `Congratulations, All Tests have Passed! \nPlease find the complete report [here](https://github.com/organization/repo/pull/3/checks?check_run_id=1121312312)  \n  ✅ 0 / 0 **Requests Passed** \n  ❌ 0 / 0 **Requests Failed**\n  ⏩ 0 / 0 **Requests Skipped**`;
         const assertions = {
             failed_count: 0,
             skipped_count: 0,
         };
         const items = [];
-        const report = {assertions, items}
-
-        const result = buildPullRequestComment(report);
+		    const report = {assertions, items}
+		    const issueNumber = 3;
+		    const checkRunId = "1121312312";
+        const result = buildPullRequestComment(report, githubService.options, issueNumber, checkRunId);
 
         expect(result).to.equal(expectedResult);
     })
 
     it("Given a collection with a passing and failing test return the correct comment", () => {
-        const expectedResult = `**Summary**:  \n  ❌ 1 **Tests Failed**\n  ⏩ 0 **Tests Skipped**\n  ✅ 1 / 2 **Reqests Passed** \n **The following tests are failing** \n\n [+] Show one place\n  -  Status code is 200 , AssertionError , expected response to have status code 200 but got 404 \n`
+        const expectedResult = 'You have some failing tests \n Please find the complete report [here](https://github.com/organization/repo/pull/3/checks?check_run_id=1121312312)  \n  ✅ 1 / 2 **Requests Passed** \n  ❌ 1 / 2 **Requests Failed**\n  ⏩ 0 / 2 **Requests Skipped** \n **The following tests are failing:** \n\n [+] Show one place\n  -  Status code is 200 , AssertionError , expected response to have status code 200 but got 404 \n'
+        const assertions = {
+            failed_count: 1,
+            skipped_count: 0,
+        };
+        const items = [
+        {
+          request_name: 'Get all ice hockey sport places within 99km around McGill University in Montréal, Canada',
+          url: 'https://sportplaces.api.decathlon.com/api/v1/places?origin=-73.582,45.511&radius=99&sports=175',
+          method: 'GET',
+          status: 'OK',
+          code: 200,
+          test_status: 'PASS',
+          failed: [],
+          skipped: []
+        },
+        {
+          request_name: 'Show one place',
+          url: 'https://sportplaces.api.decathlon.com/api/v1/places/8b1e3027-e438-42c2-92ab-5ebd23f68d54',
+          method: 'GET',
+          status: 'Not Found',
+          body: `{ "error": "Not found" }`,
+          code: 404,
+          test_status: 'FAIL',
+          failed: ["Status code is 200 , AssertionError , expected response to have status code 200 but got 404"],
+          skipped: []
+        }
+        ];
+
+        const report = {assertions, items}
+		    const issueNumber = 3;
+		    const checkRunId = "1121312312";
+        const result = buildPullRequestComment(report, githubService.options, issueNumber, checkRunId);
+
+        expect(result).to.equal(expectedResult);
+    })
+
+	it("Given a github service, issue number and checkRunId it should return the correct url from buildCheckPageUrl", () => {
+		const expectedUrl = "https://github.com/organization/repo/pull/3/checks?check_run_id=1121312312";
 
         const assertions = {
             failed_count: 1,
             skipped_count: 0,
         };
         const items = [
-{
+		{
 			request_name: 'Get all ice hockey sport places within 99km around McGill University in Montréal, Canada',
 			url: 'https://sportplaces.api.decathlon.com/api/v1/places?origin=-73.582,45.511&radius=99&sports=175',
 			method: 'GET',
@@ -248,22 +297,15 @@ describe("Utils : buildPullRequestComment", () => {
 			test_status: 'PASS',
 			failed: [],
 			skipped: []
-		},
-		{
-			request_name: 'Show one place',
-			url: 'https://sportplaces.api.decathlon.com/api/v1/places/8b1e3027-e438-42c2-92ab-5ebd23f68d54',
-			method: 'GET',
-			status: 'Not Found',
-			body: `{ "error": "Not found" }`,
-			code: 404,
-			test_status: 'FAIL',
-			failed: ["Status code is 200 , AssertionError , expected response to have status code 200 but got 404"],
-			skipped: []
-		}
-        ];
-        const report = {assertions, items}
-        const result = buildPullRequestComment(report);
+		}];
 
-        expect(result).to.equal(expectedResult);
-    })
+        const report = {assertions, items}
+		const issueNumber = 3;
+		const checkRunId = "1121312312";
+
+		const output = buildPullRequestComment(report, githubService.options, issueNumber, checkRunId).substring(80, 154)
+		expect(expectedUrl).to.equal(output);
+
+	})
+
 })
